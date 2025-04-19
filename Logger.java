192acffa03768146ee277b9b32c7c6ddf328e177
NursayatTulen <nuralemtulen@gmail.com>
2025-04-19 17:43:31 +0600

    Created Logger for tracking handler logs

--- Logger.java
+++ Logger.java
@@ -1,33 +1,5 @@
-// src/utils/Logger.java
-package utils;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class Logger {
-    private static Logger instance;
-    private List<String> logs;
-
-    private Logger() {
-        logs = new ArrayList<>();
-    }
-
-    public static Logger getInstance() {
-        if (instance == null) {
-            instance = new Logger();
-        }
-        return instance;
-    }
-
-    public void log(String handler, String message) {
-        String log = "[" + handler + "] " + message;
-        logs.add(log);
-    }
-
-    public void printLogs() {
-        System.out.println("\n--- Log History ---");
-        for (String log : logs) {
-            System.out.println(log);
-        }
+class Logger {
+    public void log(String message) {
+        System.out.println("Log: " + message);
     }
 }
