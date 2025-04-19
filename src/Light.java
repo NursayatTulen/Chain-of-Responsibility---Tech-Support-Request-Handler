79dfa709072d0dc56fd8e06236a23816daf34f72
NursayatTulen <nuralemtulen@gmail.com>
2025-04-19 19:15:35 +0600

    Added Light.java

---
+++ src/Light.java
@@ -0,0 +1,18 @@
+package src;
+public class Light {
+    private boolean isOn = false;
+
+    public void turnOn() {
+        isOn = true;
+        System.out.println("[Light] Turning ON");
+    }
+
+    public void turnOff() {
+        isOn = false;
+        System.out.println("[Light] Turning OFF");
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
+}
