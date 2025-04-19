35707737518cb5831a6bc7f8ef13c8f82a65dd7c
NursayatTulen <nuralemtulen@gmail.com>
2025-04-19 19:16:01 +0600

    Added SetThermostatCommand.java

---
+++ src/SetThermostatCommand.java
@@ -0,0 +1,21 @@
+public class SetThermostatCommand implements Command {
+    private Thermostat thermostat;
+    private int temperature;
+    private int previousTemperature;
+
+    public SetThermostatCommand(Thermostat thermostat, int temperature) {
+        this.thermostat = thermostat;
+        this.temperature = temperature;
+    }
+
+    @Override
+    public void execute() {
+        previousTemperature = thermostat.getTemperature();
+        thermostat.setTemperature(temperature);
+    }
+
+    @Override
+    public void undo() {
+        thermostat.revertTemperature();
+    }
+}
